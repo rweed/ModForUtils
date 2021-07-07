aa58d5ddb4edcc674d21d51e8faad95307e6e0c8
rweed <rweed@cavs.msstate.edu>
2021-07-07 10:09:49 -0500

    Update sortUtils.F90

--- sorters/sortUtils.F90
+++ sorters/sortUtils.F90
@@ -1,3 +1,48 @@
+
+!Copyright (C) 2021 Richard Weed.
+!All rights reserved.
+
+!Redistribution and use in source and binary forms, with or without 
+!modification, are permitted provided that the following conditions are met:
+
+!1. Redistributions of source code, in whole or in part, must retain the  
+!above copyright notice, this list of conditions and the following 
+!disclaimer.
+
+!2. Redistributions in binary form, in whole or in part, must reproduce the 
+!above copyright notice, this list of conditions and the following disclaimer 
+!in the documentation and/or other materials provided with the distribution.
+
+!3. The names of the contributors may not be used to endorse or promote from 
+!products derived from this software without specific prior written 
+!permission.
+
+!4. Redistributions of this software, in whole or in part, in any form, 
+!must be freely available and licensed under this original License. The 
+!U.S. Government may add additional restrictions to their modified and 
+!redistributed software as required by Law. However, these restrictions 
+!do not apply to the original software distribution.
+ 
+!5. Redistribution of this source code, including any modifications, may 
+!not be intentionally obfuscated.
+
+!6. Other code may make use of this software, in whole or in part, without 
+!restriction, provided that it does not apply any restriction to this 
+!software other than outlined above.
+
+!THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+!IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+!THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+!PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND
+!CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+!EXEMPLARARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+!PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+!OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+!WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+!OTHERWISE), ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+!ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ 
 Module sortUtils
 
 ! Sorting utils. Insertion short for short lists plus mergeSort and heapsorrt
